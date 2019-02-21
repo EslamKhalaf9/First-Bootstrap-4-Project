52266705856b1a0fa3b86b0f9b29bee958969cdd
eslamkhalaf9 <eslamkhalaf191@gmail.com>
2019-02-22 01:38:44 +0200

    Add Work Section

--- js/main.js
+++ js/main.js
@@ -24,5 +24,46 @@ window.addEventListener('resize', () => {
   thirdElement.style.height = actualheight + 'px';
 });
 
+// Work Section 
+// Shuffle
 
+const ul = document.querySelector('#work ul'),
+      img = document.querySelectorAll('#work .img'),
+      categories = ['Website', 'logo', 'graphic', 'marketing', 'videos'];
+
+
+ul.addEventListener('click', e => {
+  const element = e.target;
+  if(element.hasAttribute('data')){
+    /*First Of All hundle active class*/
+    if(!element.classList.contains('active')){
+      const lis = document.querySelectorAll('#work ul li');
+      lis.forEach(li => {
+        if(li.classList.contains('active')){
+          li.classList.remove('active');
+        }
+      })
+      element.classList.add('active');
+      // shuffle
+      shuffle(element,categories);
+    }
+  }
+});
+/************************************************************************* */
+function shuffle(element,array) {
+  array.forEach(cat => {
+    if(element.getAttribute('data') === 'all'){
+      img.forEach(im => {
+        im.style.opacity = '1';
+      })
+    } else if (element.getAttribute('data') === cat){
+      img.forEach(im => {
+        im.style.opacity = '1';
+        if(!im.classList.contains(cat)){
+          im.style.opacity = '0.09';
+        }
+      })
+    }
+  });
+}
 
